395955c923cb974f26f03cd050cf97f31e2b454a
Jatin Kumar <main.jatink@gmail.com>
2023-05-16 23:07:46 +0530

    booking page done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:zappe/contexts/controller.dart';
 import 'package:zappe/pages/home_page.dart';
+import 'package:zappe/screens/booking/booking.dart';
+import 'package:zappe/screens/payment/payment.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,13 +14,13 @@ void main() {
       theme: ThemeData(
         primaryColor: const Color(0xFF37CAEC),
         useMaterial3: true,
-        textTheme: GoogleFonts.outfitTextTheme(),
+        fontFamily: GoogleFonts.outfit().fontFamily,
       ),
       initialBinding: BindingsBuilder(() {
         Get.put(Controller());
       }),
       // home: Splash(),
-      home: const HomePage(),
+      home: Booking(),
     ),
   );
 }
