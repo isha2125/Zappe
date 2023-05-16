d638b1d8cf24567a3b0d129a617294bc94387b35
Jatin Kumar <main.jatink@gmail.com>
2023-05-17 02:19:49 +0530

    linking done almost

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,15 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:zappe/contexts/controller.dart';
 import 'package:zappe/pages/home_page.dart';
+import 'package:zappe/pages/shops_page.dart';
+import 'package:zappe/pages/subscription_page.dart';
+import 'package:zappe/screens/booking/booking.dart';
+import 'package:zappe/screens/drw_scr/about.dart';
+import 'package:zappe/screens/drw_scr/faqs.dart';
+import 'package:zappe/screens/drw_scr/referal.dart';
+import 'package:zappe/screens/drw_scr/trips.dart';
+import 'package:zappe/screens/payment/payment.dart';
+import 'package:zappe/screens/splash/splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,8 +26,19 @@ void main() {
       initialBinding: BindingsBuilder(() {
         Get.put(Controller());
       }),
-      // home: Splash(),
-      home: const HomePage(),
+      routes: {
+        "/": (context) => const HomePage(),
+        "/booking": (context) => Booking(),
+        "/payment": (context) => const Payment(),
+        "/splash": (context) => Splash(),
+        "/subscriptions": (context) => const Subscription(),
+        "/faq": (context) => const FaqsPage(),
+        "/about": (context) => const AboutPage(),
+        "/trips": (context) => const TripsPage(),
+        "/shop": (context) => const Shops(),
+        "/referal": (context) => const ReferalPage(),
+      },
+      initialRoute: "/login",
     ),
   );
 }
