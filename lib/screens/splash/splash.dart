e4da9885f27ae01174e76087bb16b70acbcb6fd0
flaminshinjan <srikpatra.2008@gmail.com>
2023-05-18 12:17:34 +0530

    Fixed

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -5,15 +5,27 @@ import 'package:get/get.dart';
 import 'package:rive/rive.dart';
 import 'package:zappe/screens/auth/login.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   Splash({super.key}) {
-    Timer(const Duration(seconds: 3), () {
+    timer = Timer(const Duration(seconds: 3), () {
       Get.offAll(
         () => const Login(),
         transition: Transition.rightToLeft,
         duration: const Duration(milliseconds: 1000),
       );
     });
+  }
+
+  Timer? timer;
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void dispose() {
+    widget.timer?.cancel();
+    super.dispose();
   }
 
   @override
